1f6c2b06cede8bfe3ae788496ce082d004d2ede1
Shalgin Stanislav <s.shalgin@gmail.com>
2022-02-01 21:22:42 +0300

    feat(assetCell): add custom class AssetCell for table view cell

---
+++ Investment_Assets/CellContents/AssetCell.swift
@@ -0,0 +1,15 @@
+//
+//  AssetCell.swift
+//  Investment_Assets
+//
+//  Created by Станислав on 01.02.2022.
+//
+
+import UIKit
+
+class AssetCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
