89549665e864eb01354169ddf41b14862d993ad8
Shalgin Stanislav <s.shalgin@gmail.com>
2022-02-01 20:28:51 +0300

    feat(fetch): add fetch data

--- Investment_Assets/View Controllers/ViewController.swift
+++ Investment_Assets/View Controllers/ViewController.swift
@@ -9,7 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var share: Share!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        NetworkManager.shared.fetch(from: Link.api.rawValue) { result in
+            switch result {
+            case .success(let share):
+                self.share = share
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 }
