8dc877e1fa90550c2c0f783a040be62dc2bf5d61
Shalgin Stanislav <s.shalgin@gmail.com>
2022-02-01 20:07:00 +0300

    fix(model): update api link

--- Investment_Assets/Models/Share.swift
+++ Investment_Assets/Models/Share.swift
@@ -5,19 +5,19 @@
 //  Created by Станислав on 30.01.2022.
 //
 
-struct Share {
+struct Share: Decodable {
     let securities: Securities
     let marketdata: Marketdata
 }
 
-struct Securities {
-    let data: [[Any]]
+struct Securities: Decodable {
+    let data: [[String]]
 }
 
-struct Marketdata {
-    let data: [[Any]]
+struct Marketdata: Decodable {
+    let data: [[Double]]
 }
 
 enum Link: String {
-    case api = "https://iss.moex.com/iss/engines/stock/markets/shares/boards/TQBR/securities/VTBR/.json?iss.meta=off&iss.only=securities,marketdata&marketdata.columns=SECID,LAST&securities.columns=SHORTNAME"
+    case api = "https://iss.moex.com/iss/engines/stock/markets/shares/boards/TQBR/securities/VTBR/.json?iss.meta=off&iss.only=securities,marketdata&marketdata.columns=LAST&securities.columns=SHORTNAME,SECID"
 }
